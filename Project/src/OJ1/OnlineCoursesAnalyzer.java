64b804d8a72b97b1214241f2b7ea6e91c4fd7b92
WhatWeEat <1053246332@qq.com>
2023-03-13 10:57:49 +0800

    Finish Q1,Q2 and pass the local test

--- Project/src/OJ1/OnlineCoursesAnalyzer.java
+++ Project/src/OJ1/OnlineCoursesAnalyzer.java
@@ -1,38 +1,148 @@
 package OJ1;
+import static java.lang.String.format;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * This is just a demo for you, please run it on JDK17.
+ * This is just a demo, and you can extend and implement functions
+ * based on this demo, or implement it in a different way.
+ */
 public class OnlineCoursesAnalyzer {
 
+    List<Course> courses = new ArrayList<>();
+
     public OnlineCoursesAnalyzer(String datasetPath) {
+        BufferedReader br = null;
+        String line;
+        try {
+            br = new BufferedReader(new FileReader(datasetPath, StandardCharsets.UTF_8));
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] info = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
+                Course course = new Course(info[0], info[1], new Date(info[2]), info[3], info[4], info[5],
+                    Integer.parseInt(info[6]), Integer.parseInt(info[7]), Integer.parseInt(info[8]),
+                    Integer.parseInt(info[9]), Integer.parseInt(info[10]), Double.parseDouble(info[11]),
+                    Double.parseDouble(info[12]), Double.parseDouble(info[13]), Double.parseDouble(info[14]),
+                    Double.parseDouble(info[15]), Double.parseDouble(info[16]), Double.parseDouble(info[17]),
+                    Double.parseDouble(info[18]), Double.parseDouble(info[19]), Double.parseDouble(info[20]),
+                    Double.parseDouble(info[21]), Double.parseDouble(info[22]));
+                courses.add(course);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
+    //1
     public Map<String, Integer> getPtcpCountByInst() {
-        return new HashMap<>();
+        return courses.stream().sorted((s1,s2)-> s2.institution.compareTo(s1.institution)).collect(Collectors.groupingBy(i->i.institution,Collectors.summingInt(i->i.participants)));
     }
 
+    //2
     public Map<String, Integer> getPtcpCountByInstAndSubject() {
-        return new HashMap<>();
+        return courses.stream().collect(Collectors.groupingBy(i-> format("%s-%s",i.institution.toString(),i.subject.toString()),Collectors.summingInt(i->i.participants)))
+            .entrySet().stream().sorted((e1,e2)-> {if(e2.getValue().compareTo(e1.getValue())>0) return 1;
+                else if(e2.getValue().equals(e1.getValue())) return e2.getKey().compareTo(e1.getKey());
+                else return -1;}
+            ).collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(e1, e2) -> e1, LinkedHashMap::new));
     }
-
+    //3
     public Map<String, List<List<String>>> getCourseListOfInstructor() {
-        return new HashMap<>();
+        return null;
     }
 
+    //4
     public List<String> getCourses(int topK, String by) {
-        return new ArrayList<>();
+        return null;
     }
 
-    public List<String> searchCourses(String courseSubject, double
-        percentAudited, double totalCourseHours) {
-        return new ArrayList<>();
+    //5
+    public List<String> searchCourses(String courseSubject, double percentAudited, double totalCourseHours) {
+        return null;
     }
 
-    public List<String> recommendCourses(int age, int gender, int
-        isBachelorOrHigher) {
-        return new ArrayList<>();
+    //6
+    public List<String> recommendCourses(int age, int gender, int isBachelorOrHigher) {
+        return null;
+    }
+
+}
+class Course {
+    String institution;
+    String number;
+    Date launchDate;
+    String title;
+    String instructors;
+    String subject;
+    int year;
+    int honorCode;
+    int participants;
+    int audited;
+    int certified;
+    double percentAudited;
+    double percentCertified;
+    double percentCertified50;
+    double percentVideo;
+    double percentForum;
+    double gradeHigherZero;
+    double totalHours;
+    double medianHoursCertification;
+    double medianAge;
+    double percentMale;
+    double percentFemale;
+    double percentDegree;
+
+    public Course(String institution, String number, Date launchDate,
+        String title, String instructors, String subject,
+        int year, int honorCode, int participants,
+        int audited, int certified, double percentAudited,
+        double percentCertified, double percentCertified50,
+        double percentVideo, double percentForum, double gradeHigherZero,
+        double totalHours, double medianHoursCertification,
+        double medianAge, double percentMale, double percentFemale,
+        double percentDegree) {
+        this.institution = institution;
+        this.number = number;
+        this.launchDate = launchDate;
+        if (title.startsWith("\"")) title = title.substring(1);
+        if (title.endsWith("\"")) title = title.substring(0, title.length() - 1);
+        this.title = title;
+        if (instructors.startsWith("\"")) instructors = instructors.substring(1);
+        if (instructors.endsWith("\"")) instructors = instructors.substring(0, instructors.length() - 1);
+        this.instructors = instructors;
+        if (subject.startsWith("\"")) subject = subject.substring(1);
+        if (subject.endsWith("\"")) subject = subject.substring(0, subject.length() - 1);
+        this.subject = subject;
+        this.year = year;
+        this.honorCode = honorCode;
+        this.participants = participants;
+        this.audited = audited;
+        this.certified = certified;
+        this.percentAudited = percentAudited;
+        this.percentCertified = percentCertified;
+        this.percentCertified50 = percentCertified50;
+        this.percentVideo = percentVideo;
+        this.percentForum = percentForum;
+        this.gradeHigherZero = gradeHigherZero;
+        this.totalHours = totalHours;
+        this.medianHoursCertification = medianHoursCertification;
+        this.medianAge = medianAge;
+        this.percentMale = percentMale;
+        this.percentFemale = percentFemale;
+        this.percentDegree = percentDegree;
     }
 }
